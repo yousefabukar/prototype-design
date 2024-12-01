ca118863c162889a779f964c923e089c2892d8cc
ssight <ssight@protonmail.com>
2024-12-01 19:31:07 +0000

    Add structs for image

---
+++ libcontainer/shared/src/image.rs
@@ -0,0 +1,27 @@
+use serde::{Deserialize, Serialize};
+use std::path::PathBuf;
+
+pub struct ImageOptions {
+    pub cpus: u8,
+    pub mem: usize,
+    pub manifest_path: PathBuf,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct InternalImage {
+    pub extraction_path: PathBuf,
+    pub test_script: PathBuf,
+    pub test_manifest: PathBuf,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct TestManifest {
+    pub output: String,
+    pub tests: Vec<TestOutput>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct TestOutput {
+    pub total_marks: usize,
+    pub final_marks: usize,
+}
