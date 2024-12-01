1f8d9877163376400de959024aeb2ca89e21a7a7
Manjeev Takhar <45573549+mxnjv@users.noreply.github.com>
2024-12-01 17:56:39 +0000

    Seclusion of assignments for testing purposes & simplicity + simple styling.

---
+++ app/src/assignments.js
@@ -0,0 +1,32 @@
+// Dummy data in a static array for the storage of the assignment.
+// separate .js file for simplicity
+
+let assignments = [
+    {
+        id: 1,
+        title: "Homework",
+        module: "Functional Programming",
+        dueDate: "28-11-2024",
+        hasTests: "No",
+        hasSkeletonCode: "Yes"
+    },
+    {
+        id: 2,
+        title: "News Classifier",
+        module: "Object Oriented Programming",
+        dueDate: "17-1-2024",
+        hasTests: "Yes",
+        hasSkeletonCode: "Yes"
+    },
+    {
+        id: 3,
+        title: "Server & Client Firewall Rules",
+        module: "Operating Systems & System Programming",
+        dueDate: "19-10-2024",
+        hasTests: "Yes",
+        hasSkeletonCode: "Yes"
+    },
+
+];
+
+export default assignments;
