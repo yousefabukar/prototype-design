486cf8e37484d308cd7f3a1466ee99a2f8e55597
ssight <ssight@protonmail.com>
2024-12-02 17:08:29 +0000

    bugfix: Add deserialise trait to engine structs

--- libcontainer/shared/src/engine.rs
+++ libcontainer/shared/src/engine.rs
@@ -1,5 +1,5 @@
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 use zbus::zvariant::Type;
 
-#[derive(Serialize, Type)]
+#[derive(Serialize, Deserialize, Type)]
 pub struct ContainerHandle(pub String);
