ed5eb43bd1c6e465b6931344bfbb5772501cb1af
ssight <ssight@protonmail.com>
2024-12-02 16:50:02 +0000

    feat: Add dbus typing to shared structs

--- libcontainer/shared/src/image.rs
+++ libcontainer/shared/src/image.rs
@@ -1,20 +1,22 @@
 use serde::{Deserialize, Serialize};
 use std::path::PathBuf;
+use zbus::zvariant::Type;
 
+#[derive(Type, Serialize, Deserialize)]
 pub struct ImageOptions {
     pub cpus: u8,
     pub mem: u64,
     pub manifest_path: PathBuf,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Type, Serialize, Deserialize)]
 pub struct ImageManifest {
     pub extraction_dir: PathBuf,
     pub test_script: PathBuf,
     pub test_manifest: PathBuf,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Type, Serialize, Deserialize)]
 pub struct TestOutput {
     pub passed: bool,
     pub weight: usize,
