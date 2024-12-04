2a117806318019b99d1bd3a3d13174a29d46e9b8
ssight <ssight@protonmail.com>
2024-12-04 17:59:34 +0000

    feat: Add extraction to image handle

--- libcontainer/bridge/src/error.rs
+++ libcontainer/bridge/src/error.rs
@@ -5,4 +5,8 @@ use thiserror::Error;
 pub enum ContainerError {
     #[error("The libcontainer daemon process panicked: {0}")]
     EngineService(ServiceError),
+    #[error("Invalid file location specified - file not found")]
+    FileNotFound,
+    #[error("Failed to unpack archive")]
+    ExtractionFailure,
 }
--- libcontainer/bridge/src/image.rs
+++ libcontainer/bridge/src/image.rs
@@ -1,5 +1,10 @@
 use crate::error::ContainerError;
+use flate2::read::GzDecoder;
+use std::env;
+use std::fs::File;
 use std::path::PathBuf;
+use tar::Archive;
+use uuid::Uuid;
 
 pub struct ContainerImg {
     path: PathBuf,
@@ -11,6 +16,17 @@ impl ContainerImg {
     }
 
     pub fn extract(&mut self) -> Result<(), ContainerError> {
-        todo!()
+        let target_dir = env::temp_dir().join(Uuid::new_v4().to_string());
+
+        let archive_ptr = File::open(&self.path).map_err(|_| ContainerError::FileNotFound)?;
+        let decoder = GzDecoder::new(archive_ptr);
+
+        let mut archive = Archive::new(decoder);
+        archive
+            .unpack(env::temp_dir().join(&target_dir))
+            .map_err(|_| ContainerError::ExtractionFailure)?;
+
+        self.path = target_dir;
+        Ok(())
     }
 }
