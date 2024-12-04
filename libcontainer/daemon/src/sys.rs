c18ea95a998cf652475f15b956fe0e9bda6e3b02
ssight <ssight@protonmail.com>
2024-12-04 18:19:03 +0000

    feat: Add unit tests to daemon

--- libcontainer/daemon/src/sys.rs
+++ libcontainer/daemon/src/sys.rs
@@ -21,3 +21,29 @@ impl SchedulerIface {
         Ok(())
     }
 }
+
+#[test]
+fn test_max_cpu() {
+    use std::process;
+    assert!(SchedulerIface::set_max_cpus(process::id(), 2).is_ok());
+}
+
+#[test]
+#[allow(clippy::unwrap_used)]
+fn test_max_mem() {
+    use nix::errno::Errno;
+
+    let err = SchedulerIface::set_max_mem(256_000_000).err().unwrap();
+
+    // Tests are not run as root, so this *should* result in a permissions error
+    let EngineError::NixSys(Errno::EPERM) = err else {
+        panic!("Didn't get the expected error");
+    };
+}
+
+#[test]
+fn test_uuid() {
+    use nix::unistd::Uid;
+    assert!(!Uid::current().is_root());
+    assert!(!Uid::effective().is_root());
+}
