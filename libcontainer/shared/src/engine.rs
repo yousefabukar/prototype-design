95534a0393bfa3b47746a7a7962650745b25c372
ssight <ssight@protonmail.com>
2024-12-04 15:19:30 +0000

    chore: Derive clone on container handle pointer

--- libcontainer/shared/src/engine.rs
+++ libcontainer/shared/src/engine.rs
@@ -1,5 +1,5 @@
 use serde::{Deserialize, Serialize};
 use zbus::zvariant::Type;
 
-#[derive(Serialize, Deserialize, Type)]
+#[derive(Serialize, Deserialize, Type, Clone)]
 pub struct ContainerHandle(pub String);
