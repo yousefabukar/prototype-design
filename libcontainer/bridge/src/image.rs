32c3a0214e72b21cca0aa616fbd1208495708e7a
ssight <ssight@protonmail.com>
2024-12-04 17:54:54 +0000

    feat: Create initial image stubs

--- libcontainer/bridge/src/image.rs
+++ libcontainer/bridge/src/image.rs
@@ -1,1 +1,16 @@
-pub struct ContainerImg {}
+use crate::error::ContainerError;
+use std::path::PathBuf;
+
+pub struct ContainerImg {
+    path: PathBuf,
+}
+
+impl ContainerImg {
+    pub fn new(path: PathBuf) -> Self {
+        ContainerImg { path }
+    }
+
+    pub fn extract(&mut self) -> Result<(), ContainerError> {
+        todo!()
+    }
+}
