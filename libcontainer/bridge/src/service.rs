595de74ece7dd72ddecbb2bbe44dd3091694207e
ssight <ssight@protonmail.com>
2024-12-04 15:11:47 +0000

    bufix: Fix public function in trait

--- libcontainer/bridge/src/service.rs
+++ libcontainer/bridge/src/service.rs
@@ -1,26 +1,21 @@
 use shared::engine::ContainerHandle;
 use shared::error::ServiceError;
-use zbus::{proxy, Connection};
+use shared::image::{ImageOptions, TestOutput};
+use std::path::PathBuf;
+use zbus::proxy;
 
 #[proxy(
     interface = "libcontainer.daemon",
     default_service = "libcontainer.daemon",
     default_path = "/libcontainer/daemon"
 )]
-trait ContainerManager {
-    pub async fn new_container(
+pub trait ContainerManager {
+    async fn new_container(
         &self,
         opts: ImageOptions,
         img_path: PathBuf,
     ) -> Result<ContainerHandle, ServiceError>;
 
-    pub async fn wait_for_completion(
-        &self,
-        handle: ContainerHandle,
-    ) -> Result<String, ServiceError>;
-
-    pub async fn test_output(
-        &self,
-        handle: ContainerHandle,
-    ) -> Result<Vec<TestOutput>, ServiceError>;
+    async fn wait_for_completion(&self, handle: ContainerHandle) -> Result<String, ServiceError>;
+    async fn test_output(&self, handle: ContainerHandle) -> Result<Vec<TestOutput>, ServiceError>;
 }
