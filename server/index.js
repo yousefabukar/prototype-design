70a2982d8fcfb6c3063ab88bdb27fa71e12b98aa
Nisar Ahmed <juniorchannel54@gmail.com>
2024-12-10 11:26:39 +0000

    assignments interface commands completed

--- server/index.js
+++ server/index.js
@@ -16,6 +16,28 @@ app.get('/api/assignments', async (req, res) => {
     }
 });
 
+app.post('/api/assignments', async (req, res) => {
+    try{
+        const { 
+            assignment_title, module_name, due_date, image_filepath, vcpu_value, memory_value} = req.body;
+        
+        const [result] = await db.query (
+            'INSERT INTO assignments (assignment_title, module_name, due_date, image_filepath, vcpu_value, memory_value) VALUES (?, ?, ?, ?, ?, ?)',
+            [assignment_title, module_name, due_date, image_filepath, vcpu_value, memory_value]
+        );
+
+        res.status(201).json({ 
+            id: result.insertId,
+            message: 'Assignment created successfully'
+        });
+    } catch (err) {
+        console.error('Error creating assignment:', err);
+        res.status(500).send('Error creating assignment');
+    }
+
+    });
+
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
