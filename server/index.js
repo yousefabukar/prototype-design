baf7d1094529f1b88d8ecfebec9158c169c67ba8
Yousef Abukar <abukaryousef@gmail.com>
2024-12-10 17:51:42 +0000

    accept db index js

--- server/index.js
+++ server/index.js
@@ -1,15 +1,45 @@
-const express = require('express');
-const cors = require('cors');
+import express from 'express';    
+import db from './config/db.js';
 
 const app = express();
-app.use(cors());
+
 app.use(express.json());
 
-app.get('/api/health', (req, res) => {
-    res.json({ status: 'ok' });
-  });
-  
-  const PORT = process.env.PORT || 3000;
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
+
+app.get('/api/assignments', async (req, res) => {
+    try {
+        const [assignments] = await db.query('SELECT * FROM Assignments');
+        res.json(assignments);  
+    } catch (err) {
+        console.error('Error fetching assignments:', err);
+        res.status(500).send('Error fetching assignments');
+    }
+});
+
+app.post('/api/assignments', async (req, res) => {
+    try{
+        const { 
+            assignment_title, module_name, due_date, image_filepath, vcpu_value, memory_value} = req.body;
+        
+        const [result] = await db.query (
+            'INSERT INTO assignments (assignment_title, module_name, due_date, image_filepath, vcpu_value, memory_value) VALUES (?, ?, ?, ?, ?, ?)',
+            [assignment_title, module_name, due_date, image_filepath, vcpu_value, memory_value]
+        );
+
+        res.status(201).json({ 
+            id: result.insertId,
+            message: 'Assignment created successfully'
+        });
+    } catch (err) {
+        console.error('Error creating assignment:', err);
+        res.status(500).send('Error creating assignment');
+    }
+
+    });
+
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
